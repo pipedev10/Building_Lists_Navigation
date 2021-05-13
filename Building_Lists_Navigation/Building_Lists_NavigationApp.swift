19f4370ba2995920e4e6e68f530b8fb918a49c7c
Felipe Carrasco <felipe@bakers.dev>
2021-05-12 21:17:06 -0400

    Initial Commit

---
+++ Building_Lists_Navigation/Building_Lists_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Building_Lists_NavigationApp.swift
+//  Building_Lists_Navigation
+//
+//  Created by Pipe Carrasco on 12-05-21.
+//
+
+import SwiftUI
+
+@main
+struct Building_Lists_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
